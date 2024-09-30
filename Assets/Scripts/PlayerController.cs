ed4e68a57a32d0c2a7d7d75a21183889bd41b651
Yaqu0b <yaquobrahman@gmail.com>
2024-09-30 20:14:49 +0100

    Lab 2 - 2.2 Scripting
    Picking and Counting Pick-ups

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,14 @@ public class PlayerController : MonoBehaviour {
 
     public Vector2 moveValue;
     public float speed;
+    private int count;
 
-    void OnMove (InputValue value) {
+    void Start() {
+        count = 0;
+        Debug.Log("Pickup count: " + count);
+    }
+
+    void OnMove(InputValue value) {
         moveValue = value.Get<Vector2>();
     }
 
@@ -16,5 +22,16 @@ public class PlayerController : MonoBehaviour {
         Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
 
         GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime) ;
+    }
+
+    // This method is called when the player collides with an object
+    void OnTriggerEnter(Collider other) {
+        // Check if the object is tagged as "Pickup"
+        if(other.gameObject.tag == "PickUp") {
+            // Increment the count by 1 when a pickup is collected
+            count += 1;
+            Debug.Log("Pickup collected! Total count: " + count);
+            other.gameObject.SetActive(false);
+        }
     }
 }
